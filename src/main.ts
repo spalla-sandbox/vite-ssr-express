02de7a6c3a99bc38e1c69076d4b5fa6385d998d3
Igor Jacaúna <igor.jacauna@gmail.com>
2023-07-17 09:54:11 -0400

    feat: melhora busca por rotas

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import getRouter from './router';
-import NotFound from './pages/[...all].page';
 import { PageContext } from './types';
 
 export async function render(url: string, context: PageContext) {
   const router = await getRouter()
   const match = router.lookup(`/${url}`)
-  if (match) {
-    const page = await match.handler()
-    return page(match.params, context)
-  }
-  return NotFound()
+  const page = await match.handler()
+  return page(match.params, context)
 }
