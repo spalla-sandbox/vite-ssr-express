af3cf27fcf125260d73163ab5d8a4a1d48dc0985
Igor Jacaúna <igor.jacauna@gmail.com>
2023-07-20 00:02:14 -0400

    chore: Corrige resposta em prod

--- server/handlers/prodHandler.js
+++ server/handlers/prodHandler.js
@@ -6,12 +6,16 @@ export default async function productionHandler(app) {
   const compression = (await import('compression')).default
   app.use(compression())
 
-  return async (req, res) => {
+  return async (req, res, next) => {
     try {
       const url = req.originalUrl.replace(getBaseURL(), '')
       const render = (await import(path.resolve('.', 'output/server/main.js'))).render
       const html = await render(url, { req, res })
-      res.set({ 'Content-Type': 'text/html' }).end(html)
+      if (!res.headersSent) {
+        res.set({ 'Content-Type': 'text/html' }).end(html)
+      } else {
+        next()
+      }
     } catch (e) {
       console.log(e.stack)
       res.status(500).end(e.stack)
