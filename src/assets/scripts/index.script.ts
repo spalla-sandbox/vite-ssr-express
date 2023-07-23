1e95609d67d52769ee06e3b47fe608c69ad77686
Igor Jacaúna <igor.jacauna@gmail.com>
2023-07-23 17:29:42 -0400

    chore: Adiciona exemplo de importação dinâmica

--- src/assets/scripts/index.script.ts
+++ src/assets/scripts/index.script.ts
@@ -1,11 +1,10 @@
-import { format } from 'date-fns';
-
 const button = document.querySelector('button');
 let count = 0;
 
 button.textContent = `Clicked ${count} time(s)`;
 
-button.onclick = () => {
+button.onclick = async () => {
+  const { format } = await import('date-fns');
   count += 1;
   button.textContent = `Clicked ${count} time(s) at ${format(
     new Date(),
