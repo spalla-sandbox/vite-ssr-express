0885df9c6d4931b3912aacb752d9566b18058967
Igor Jacaúna <igor.jacauna@gmail.com>
2023-07-31 10:49:01 -0400

    doc: Documenta arquivo de redirects

--- src/redirects.ts
+++ src/redirects.ts
@@ -1,4 +1,12 @@
+/**
+ * Center predefined redirects
+ * {
+ *   <route>: <route to redirect> || [<route to redirect>, <optional status code>]
+ * }
+ *
+ * When no status code is provided, 302 is used by default
+ */
 export default {
-  '/redirecionar': ['/entrar', 302],
   '/redirecionar-2': '/',
+  '/redirecionar': ['/entrar', 301],
 };
